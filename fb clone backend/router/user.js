e87ee444150c9975d9bb45c60c3b028c369f020e
Upeksha1214 <upekshasachintha@gmail.com>
2022-08-25 20:33:26 -0700

    updated

--- fb clone backend/router/user.js
+++ fb clone backend/router/user.js
@@ -1,31 +1,59 @@
-const express=require("express")
+const express=require('express')
 const router=express.Router()
-const User=require("../models/user.model")
+const User=require('../models/user.model')
 
+router.post('/login',async(req,res)=>{
+    const response= await User.findOne({email : req.body.email , password : req.body.password});
+    response!=null ? res.json({code:'200',message:'login  successfull',data:response.surname}) : 
+                    res.json({code:'500',message:'login faild',data:null})
+ 
+ })
 
-router.get('/',async(req,res)=>{
-    const response= await User.find()
-    res.json(response)
-  })
-
-router.post('/createAccount',async(req,res)=>{
-      
-    const user=new User({
-           firstName : req.body.firstName,
-           surname : req.body.surname,
-           gender : req.body.gender,
-           dateOfBirth : req.body.dateOfBirth,
-           password : req.body.password,
-           phoneNumber : req.body.phoneNumber,
-           email : req.body.email,
-        })
-      
-        try {
-           const response=await user.save();
-             res.send("user register complete")
-        } catch (error) {
-            res.send(error)
-        }
+router.put('/updateProfile/:email' ,async(req,res)=>{
+ await User.findOneAndUpdate({email : req.params.email} , req.body)
+ response!=null ? res.json({code:'200',message:'profile update successfull',data:null}) : 
+                  res.json({code:'500',message:'profile update faild',data:null})
 })
 
+router.post('/createAccount',async(req,res)=>{
+      try {
+
+    
+     const re= await User.findOne({ email: req.body.email});
+     if(re==null){
+        const user=new User({
+        firstName:req.body.firstName,
+        surname:req.body.surname,
+        gender:req.body.gender,
+        dateOfBirth:req.body.dateOfBirth,
+        phoneNumber:req.body.phoneNumber,
+        email : req.body.email,
+        password : req.body.password
+      })
+
+        const response=await user.save();
+        response != null ? res.json({code:'200',message:'Account create successfull',data:null}) : 
+                           res.json({code:'500',message:'User Account Create Fail',data:null});
+      }else{
+      res.json({code:'500',message:'Email is AllreadyExists',data:null});
+     }
+
+
+      // tools.existsUser()
+
+       
+      
+        // 
+        // 
+      
+        
+      } catch (error) {
+        res.send('Err'+error)
+      }
+})
+
+
+router.post('/login',(req,res)=>{
+    res.send('login account')
+})
 module.exports=router
